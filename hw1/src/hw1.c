c67692a79aa2affae147254d3f85b093e727f690
Zhecheng Weng <zhecheng.weng@stonybrook.edu>
2017-01-30 13:18:25 -0500

    My First Commit for hw1

--- hw1/src/hw1.c
+++ hw1/src/hw1.c
@@ -6,6 +6,19 @@
 char validargs(int argc, char** argv, FILE** in, FILE** out) {
 	char ret = 0;
 
+	if(*argv[1]=='-'){
+		argv[1]++;
+		if(*argv[1]=='h'){
+		USAGE(1);
+		return 0;
+	}
+
+	}
+
+
+
+
+
 	/* code here */
 
 	return ret;
