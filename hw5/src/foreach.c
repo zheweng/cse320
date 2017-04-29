188654da12af4e7d116537b93716596c3189fe5c
Zhecheng Weng <zhecheng.weng@stonybrook.edu>
2017-04-28 23:42:08 -0400

    final

--- hw5/src/foreach.c
+++ hw5/src/foreach.c
@@ -3,19 +3,39 @@
 #include <string.h>
 #include "foreach.h"
 
-static int loop_index;
+
+static pthread_key_t eachkey;
+static pthread_once_t eachinited=PTHREAD_ONCE_INIT;
+
+void free_eachkey(void* loop_index){
+	free(loop_index);
+}
+void init_eachkey(){
+	pthread_key_create(&eachkey,free_eachkey);
+}
+void setup_index(){
+	int* loop_index;
+	pthread_once(&eachinited,init_eachkey);
+	loop_index=(int*)malloc(sizeof(int));
+	*loop_index=0;
+	pthread_setspecific(eachkey,loop_index);
+}
+
+
 
 void *foreach_init(arraylist_t* items){
 
     if(items==NULL|| items->length==0){
     	return NULL;
     }
-    loop_index=0;
+    setup_index();
     return get_index_al(items,0);
 
 }
 
 void *foreach_next(arraylist_t* items, void* data){
+	int* indexptr=(int*)pthread_getspecific(eachkey);
+	int  loop_index= *indexptr;
 
     if(data!=NULL){
     	char* currentPtr=(char*)items->base+loop_index* items->item_size;
@@ -25,11 +45,14 @@ void *foreach_next(arraylist_t* items, void* data){
     	return NULL;
     }
     loop_index++;
+    *indexptr=loop_index;
     return get_index_al(items,loop_index);
 }
 
 size_t foreach_index(){
-   return loop_index;
+	int* indexptr=(int*)pthread_getspecific(eachkey);
+	int  loop_index= *indexptr;
+	return loop_index;
 }
 
 bool foreach_break_f(){
--- hw5/tests/al_tests.c
+++ hw5/tests/al_tests.c
@@ -63,9 +63,13 @@ void* delete_itemfromal(void* data){
 }
 void* delete_itemfromindex(void* data){
     int* num=(int*)data;
+
+
     remove_index_al(global_list,*num);
     pthread_exit(NULL);
 }
+
+
 
 
 /******************************************
@@ -91,21 +95,23 @@ Test(al_suite, 2_insertion, .timeout=2, .init=setup, .fini=teardown){
     cr_assert(true, "I win");
 }
 
-Test(al_suite, 3_removal, .timeout=2, .init=setup, .fini=teardown){
-    int  num_items=200;
+Test(al_suite,3_deletion, .timeout=2, .init=setup, .fini=teardown){
+    int num_items=200;
     test_item_t* items[num_items];
-    for(int i=0;i<num_items;i++){
-        items[i]=(test_item_t*)malloc(sizeof(test_item_t));
-        items[i]->i=i;
+    for(int i=0; i< num_items;i++){
+        items[i] = (test_item_t*)malloc(sizeof(test_item_t));
+        items[i]->i = i;
         items[i]->some_data = malloc(1);
     }
-    for(int i=0;i<num_items;i++){
+     for(int i=0;i<num_items;i++){
         insert_al(global_list,items[i]);
     }
 
     cr_assert(global_list->length==num_items,"test 3: wrong length after insertion");
-    pthread_t pids[num_items];
+     pthread_t pids[num_items];
     for(int i=0;i<num_items;i++){
+
+
         pthread_create(&pids[i],NULL,delete_itemfromal,items[i]);
     }
     for(int i=0;i<num_items;i++){
@@ -115,9 +121,10 @@ Test(al_suite, 3_removal, .timeout=2, .init=setup, .fini=teardown){
     }
     cr_assert(global_list->length==0,"test3: wrong length after insertion");
 
+
 }
 
-Test(al_suite, 4_removeal, .timeout=10, .init=setup, .fini=teardown) {
+Test(al_suite, 4_deletion, .timeout=2, .init=setup, .fini=teardown) {
     int num_del_Items = 10000;
 
     test_item_t* items[num_del_Items];
@@ -138,15 +145,17 @@ Test(al_suite, 4_removeal, .timeout=10, .init=setup, .fini=teardown) {
     printf("Test4 length of list after insertion: %lu\n", global_list->length);
 
     cr_assert(global_list->length == num_del_Items, "Test 4: wrong length after insertion");
-
     pthread_t pids[num_del_Items];
 
-    for(int i = 0; i < num_del_Items; i++)
+    for(int i = 0; i < num_del_Items; i++){
         pthread_create(&pids[i], NULL, delete_itemfromindex, &rm_num);
+    }
 
-    for(int i = 0; i < num_del_Items; i++)
+    for(int i = 0; i < num_del_Items; i++){
         pthread_join(pids[i], NULL);;
+    }
 
     cr_assert(global_list->length == 0, "Test 4: wrong length after deletion");
+
 }
 
